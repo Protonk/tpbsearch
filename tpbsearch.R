d786732efb38a827d61bcb49f0959bdaccbd2a09
Adam Hyland <protonk@gmail.com>
2012-03-09 17:20:50 -0600

    correct pipe from stdin

--- tpbsearch.R
+++ tpbsearch.R
@@ -1,7 +1,14 @@
+#!/usr/bin/Rscript --no-restore --no-save 
+
+# Just pipe the arguments as you would for any script
+# e.g. grep Zoolander ~/tpbsearch/complete | ./tpbsearch.R
+# 
+
+
 library(stringr)
 
 tgrep <- function() {
-  tg <- scan("~/tpbsearch/searchtext", sep = "\n", what="character.raw", quiet = TRUE)
+  tg <- scan(pipe('cat /dev/stdin'), sep = "\n", what="character.raw", quiet = TRUE)
   tgm <- str_split_fixed(tg, "\\|", 6)[, -1]
   tgm <- data.frame(head(tgm[order(as.numeric(tgm[, 3]), decreasing=TRUE),]), stringsAsFactors= FALSE)
   tgm[ , 2] <- paste(format(as.numeric(tgm[, 2])/1024^2, digits = 3, scientific=FALSE), "Mb", sep = " ")
@@ -9,10 +16,11 @@ tgrep <- function() {
   tgm <- tgm[, -4]
   names(tgm) <- c("Title", "Size", "Seeds/Leechers", "Hash")
   magnet <- paste("magnet:?xt=urn:btih:", tgm[1, "Hash"], sep = "")
+  closeAllConnections()
   print(tgm)
-  flush.console()
   print(magnet)
 }
 
 tgrep()
+
 q()

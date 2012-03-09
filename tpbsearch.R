4ee755b32a6348f2de065686c40d57318ca12a04
Adam Hyland <protonk@gmail.com>
2012-03-09 14:20:10 -0600

    reinsert as.numeric call

--- tpbsearch.R
+++ tpbsearch.R
@@ -1,9 +1,9 @@
 library(stringr)
 
 tgrep <- function() {
-  tg <- scan("~/pbsearch/searchtext", sep = "\n", what="character.raw", quiet = TRUE)
+  tg <- scan("~/tpbsearch/searchtext", sep = "\n", what="character.raw", quiet = TRUE)
   tgm <- str_split_fixed(tg, "\\|", 6)[, -1]
-  tgm <- data.frame(head(tgm[order(tgm[, 3], decreasing=TRUE),]), stringsAsFactors= FALSE)
+  tgm <- data.frame(head(tgm[order(as.numeric(tgm[, 3]), decreasing=TRUE),]), stringsAsFactors= FALSE)
   tgm[ , 2] <- paste(format(as.numeric(tgm[, 2])/1024^2, digits = 3, scientific=FALSE), "Mb", sep = " ")
   tgm[ , 3] <- paste(tgm[ , 3], "/", tgm[ , 4], sep = "")
   tgm <- tgm[, -4]
